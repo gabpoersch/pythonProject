7d496bcbb845e70042aa26ee67e50f1707203d2f
Gabriel Poersch <gabpoersch@gmail.com>
2022-12-05 06:20:07 -0300

    first lesson done

--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('products/', include('products.urls')),
+    path('about/', include('about.urls')),
 
 ]
